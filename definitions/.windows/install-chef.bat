0cec065a26dc3232683d5e029f7855717e90f812
loadx <loadx.mat@gmail.com>
2013-05-07 00:02:58 +1000

    Removed windows definitions

--- definitions/.windows/install-chef.bat
+++
@@ -1,6 +0,0 @@
-timeout 10
-REM sleeping to give outbound networking a chance to come up
-cmd /C cscript wget.vbs /url:http://www.opscode.com/chef/install.msi /path:chef-client.msi
-cmd /C msiexec /qn /i chef-client.msi
-
-
