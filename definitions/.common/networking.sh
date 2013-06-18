528e67ba308e5d7268a2a2e9e5841bc2ab588e7c
loadx <loadx.mat@gmail.com>
2013-06-18 11:54:09 +1000

    Oops. Need to actually remove the file.

--- definitions/.common/networking.sh
+++ definitions/.common/networking.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
-if [-f /etc/udev/rules.d/70-persistent-net.rules ]; then
-    rm /etc/udev/rules.d/70-persistent-net.rules
+if [ -f /etc/udev/rules.d/70-persistent-net.rules ]; then
+    rm -rf /etc/udev/rules.d/70-persistent-net.rules
 fi
 
 mkdir /etc/udev/rules.d/70-persistent-net.rules
