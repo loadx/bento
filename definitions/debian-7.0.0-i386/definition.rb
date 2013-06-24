31591f4c4d88dcd118f8cc29082c696e88d2b4a8
Andy Zhang <aibieliandy@gmail.com>
2013-06-25 00:19:10 +1000

    Updated Debian 7.0.0 links as the old mirrors no longer function Fixed bug where Debian 7.0.0 iso was using the incorrect MD5.

--- definitions/debian-7.0.0-i386/definition.rb
+++ definitions/debian-7.0.0-i386/definition.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + "/../.debian_7/session.rb"
 
-iso = "debian-7.0.0-i386-netinst.iso"
+iso = "debian-live-7.0.0-i386-standard.iso"
 
 session =
   DEBIAN_7_SESSION.merge(:os_type_id => 'Debian',
                         :iso_file => iso,
-                        :iso_md5 => "a6b93666a5393334accb7ac4ee28d949",
-                        :iso_src => "http://cdimage.debian.org/debian-cd/7.0.0/amd64/iso-cd/#{iso}")
+                        :iso_md5 => "b504de07da1311d6022680899d1f33d1",
+                        :iso_src => "http://cdimage.debian.org/debian-cd/7.0.0-live/i386/iso-hybrid/#{iso}")
 
 Veewee::Session.declare session
--- definitions/debian-7.0.0/definition.rb
+++ definitions/debian-7.0.0/definition.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + "/../.debian_7/session.rb"
 
-iso = "debian-7.0.0-amd64-netinst.iso"
+iso = "debian-live-7.0.0-amd64-standard.iso"
 
 session =
   DEBIAN_7_SESSION.merge(:os_type_id => 'Debian',
                         :iso_file => iso,
-                        :iso_md5 => "6a55096340b5b1b7d335d5b559e13ea0",
-                        :iso_src => "http://cdimage.debian.org/debian-cd/7.0.0/amd64/iso-cd/#{iso}")
+                        :iso_md5 => "f0e0f567c7f3f963f09e8b50098fb1e1",
+                        :iso_src => "http://cdimage.debian.org/debian-cd/7.0.0-live/amd64/iso-hybrid/#{iso}")
 
 Veewee::Session.declare session
